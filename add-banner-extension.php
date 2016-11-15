7fd153bd80a9e339d827e91735f3db5745c4d505
nakayama_yoshie <yoshi.nkyma@gmail.com>
2016-11-15 20:14:46 +0900

    フォルダ名と同じファイル名のPHPファイルを作成(仕切り直し) #3

---
+++ add-banner-extension.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Add Banner Extension
+Plugin URI: https://github.com/sararilfy/add-banner-extension
+Description: (プラグインの短い説明)
+Version: 1.0.0
+Author: Yoshie Nakayama
+License: GPLv2 or later
+Text Domain: add-banner-extension
+Domain Path: /languages
+*/
+new add_Banner_Extension();
+class add_Banner_Extension {
+
+}
--- add-banner-to-post.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: aratana22
- * Date: 15/11/17
- * Time: 19:17
- */
