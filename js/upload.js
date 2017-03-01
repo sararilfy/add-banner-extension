cc32e053f3cad723209c2e523e1437fc63d7a7a0
Yoshie Nakayama <yoshi.nkyma@gmail.com>
2017-03-01 16:53:13 +0900

    Css and JS Code refactoring.

--- js/upload.js
+++ js/upload.js
@@ -1,30 +1,30 @@
 (function($) {
-    $(function() {
-        var custom_uploader = wp.media({
-            title: 'Choose Image',
-            library: {
-                type: 'image'
-            },
-            button: {
-                text: 'Choose Image'
-            },
-            multiple: false
-        });
+	$(function() {
+		var custom_uploader = wp.media({
+			title: 'Choose Image',
+			library: {
+				type: 'image'
+			},
+			button: {
+				text: 'Choose Image'
+			},
+			multiple: false
+		});
 
-        custom_uploader.on("select", function () {
-            var images = custom_uploader.state().get('selection');
+		custom_uploader.on('select', function () {
+			var images = custom_uploader.state().get('selection');
 
-            images.each(function(file) {
-                $("#banner-image-url").val(file.toJSON().url);
-                if($("#banner-image-alt").val() == "") {
-                    $("#banner-image-alt").val(file.toJSON().alt);
-                }
-                $("#banner-image-view").attr("src", file.toJSON().url);
-            });
-        });
+			images.each(function(file) {
+				$("#banner-image-url").val(file.toJSON().url);
+				if( $('#banner-image-alt').val() == '' ) {
+					$('#banner-image-alt').val(file.toJSON().alt);
+				}
+				$('#banner-image-view').attr('src', file.toJSON().url);
+			});
+		});
 
-        $("#media-upload").on("click", function() {
-            custom_uploader.open();
-        });
-    });
+		$('#media-upload').on('click', function() {
+			custom_uploader.open();
+		});
+	});
 })(jQuery);
